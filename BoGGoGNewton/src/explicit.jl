4a0e60022c50558f9af507ff9ed34641e72dfdfb
BoGGoG <marco.knipfer@me.com>
2021-06-15 10:56:50 +0200

    Fixed `newton_it`

--- BoGGoGNewton/src/explicit.jl
+++ BoGGoGNewton/src/explicit.jl
@@ -6,10 +6,10 @@ function newton_func(func, x_0::Float64; eps = 1e-5)
 end
 
 function newton_it(func::Function, x0::Float64; eps = 1e-8, maxit = 1000)
-    x1::Float64 = Inf
+    x1 = newton_step(func, x0)
     i = 0
-    while abs2(x1 - x0) < eps && i < maxit 
-        x0 = x_1
+    while abs2(x1 - x0) > eps && i < maxit
+        x0 = x1
         x1 = newton_step(func, x0)
         i += 1
     end
